cbba654e75b69df5423cde019e4be5a915df21f8
Ares Terzopoulos <cernunos_kav@msn.com>
2019-01-14 22:59:57 +0200

    final prep for publication

--- notebooks/other/Internal/convertJupyterToUmbracoRte.fsx
+++ notebooks/other/Internal/convertJupyterToUmbracoRte.fsx
@@ -1,4 +1,4 @@
-#load @"main.group.fsx"
+#load ".paket/load/main.group.fsx"
 
 open Fizzler.Systems.HtmlAgilityPack
 open HtmlAgilityPack
@@ -6,28 +6,29 @@ open System
 open System.Text.RegularExpressions
 open System.IO
 
-let convert (source:string) =
-    
+let trunc ln (str:string) =
+    let real_ln = str.Length
+    str.Substring(0, Math.Min(ln, real_ln))
+
+let convert (source : string) =
     let output = source.Replace(".html", "_output.html")
-    
     let doc = HtmlDocument()
-
     doc.Load(source)
-
     let (|TextCell|CodeCell|Other|) (node : HtmlNode) =
         match node.Attributes.["class"].Value with
         | css when css.Contains("text_cell") -> TextCell node
         | css when css.Contains("code_cell") -> CodeCell node
         | _ -> Other node
-
     doc.DocumentNode.QuerySelectorAll(".cell")
     |> Seq.collect (function
            | TextCell node ->
                let text = node.QuerySelector(".text_cell_render")
                text.Attributes.Remove("class")
                let anchor = text.QuerySelectorAll("a.anchor-link")
-               if (anchor <> null) 
-               then while anchor |> Seq.length > 0 do (anchor |> Seq.head).Remove()
+               if (anchor <> null) then
+                   while anchor
+                         |> Seq.length > 0 do
+                       (anchor |> Seq.head).Remove()
                [ text ]
            | CodeCell node ->
                let code =
@@ -40,13 +41,22 @@ let convert (source:string) =
                    match node.QuerySelector(".output_area") with
                    | null -> null
                    | node ->
-                       if node.FirstChild.Name = "#text" then node
-                       else node.FirstChild
-                       |> fun outputNode ->
-                           outputNode.OuterHtml.Replace("\r\r","\r")
-                           |> sprintf
-                                  "<div><span style=\"font-family: 'Courier New', Courier, monospace\">Output:</span>%s</div>"
-                           |> HtmlNode.CreateNode
+                       //printfn "%s, %s" (node.FirstChild.Name) (trunc 30 <| node.InnerText.Trim())
+                       if (node.Descendants("script") |> Seq.length) > 0 then
+                           HtmlNode.CreateNode
+                               "<div><span style=\"font-family: 'Courier New', Courier, monospace\">Output:</span><div style='border: solid gold 3px; border-radius: 5px; padding: 5px;'><img src='' alt='There should probably be a chart image here'</div></div>"
+                       else if node.FirstChild.Name = "#text" then
+                            let result =  Regex.Replace(node.OuterHtml, @"((\r\n|\n|\r)$)|(^(\r\n|\n|\r))|^\s*$", "", RegexOptions.Multiline)
+                            result |> trunc 200 |> printfn "[[[[ %s ]]]]"
+                            result |> HtmlNode.CreateNode
+                       else
+                           node.FirstChild
+                           |> fun outputNode ->
+                               outputNode.OuterHtml.Replace("\n\n", "\n")
+                               //Regex.Replace(node.OuterHtml, "^\\s*$", "·", RegexOptions.Multiline)
+                               |> sprintf
+                                      "<div><span style=\"font-family: 'Courier New', Courier, monospace\">Output:</span>%s</div>"
+                               |> HtmlNode.CreateNode
 
                [ code; output ]
            | _ -> [])
