b87ef23aa6f2bafb0f2dcf1bfd7cf199f61f6300
Ares Terzopoulos <cernunos_kav@msn.com>
2019-02-06 09:14:59 +0200

    graph shaping up

--- d3-jupyter/src/index.js
+++ d3-jupyter/src/index.js
@@ -7,20 +7,22 @@ import "d3-graphviz"
 var graphviz;
 
 $(document).on("INIT_D3", (e, path) => {
-    console.log("Event:", e);
+    console.log("Event:", e.type, e);
     console.log("Path:", path);
-    graphviz = d3.select(path).graphviz()
-    .transition(function () {
-        return d3.transition("main")
-        .ease(d3.easeLinear)
-        .delay(500)
-        .duration(1500);
-    });
+    graphviz = d3.select(path)
+        .attr("height", "100%")
+        .attr("width", "100%").graphviz()
+        .transition(function () {
+            return d3.transition("main")
+                .ease(d3.easeLinear)
+                .delay(500)
+                .duration(1000);
+        });
     //.logEvents(true);
 });
 
 $(document).on("RENDER_GRAPH", (e, graph) => {
-    console.log("Event:", e);
+    console.log("Event:", e.type, e);
     console.log("Graph:", graph);
 
     graphviz.renderDot(graph);
@@ -28,4 +30,18 @@ $(document).on("RENDER_GRAPH", (e, graph) => {
     //     .graphviz()
     //     .fade(false)
     //     .renderDot(graph);
+})
+
+$(document).on("RENDER_SERIES", (e, graphs) => {
+    console.log("Event:", e.type, e);
+    console.log("Graph count:", graphs.length);
+    const render = function (index) {
+        if (index < graphs.length) {
+            graphviz
+                .renderDot(graphs[index])
+                .on("end", () => render(index + 1));
+        }
+    }
+
+    render(0);
 })
